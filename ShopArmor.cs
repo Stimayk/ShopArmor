57ab08ece3e1765e8bf41778ebcb8e7460181211
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-09-25 10:20:25 +0400

    Update ShopArmor.cs

--- ShopArmor.cs
+++ ShopArmor.cs
@@ -12,7 +12,7 @@ namespace ShopArmor
         public override string ModuleName => "[SHOP] Armor";
         public override string ModuleDescription => "";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "v1.0.0";
+        public override string ModuleVersion => "v1.0.1";
 
         private IShopApi? SHOP_API;
         private const string CategoryName = "Armor";
@@ -65,7 +65,7 @@ namespace ShopArmor
             RegisterListener<Listeners.OnClientDisconnect>(playerSlot => playerArmor[playerSlot] = null!);
         }
 
-        public void OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
+        public HookResult OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
         {
             if (TryGetNumberOfArmor(uniqueName, out int Armor))
             {
@@ -75,9 +75,10 @@ namespace ShopArmor
             {
                 Logger.LogError($"{uniqueName} has invalid or missing 'armor' in config!");
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
+        public HookResult OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
         {
             if (state == 1 && TryGetNumberOfArmor(uniqueName, out int Armor))
             {
@@ -87,11 +88,13 @@ namespace ShopArmor
             {
                 OnClientSellItem(player, itemId, uniqueName, 0);
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
+        public HookResult OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
         {
             playerArmor[player.Slot] = null!;
+            return HookResult.Continue;
         }
 
         [GameEventHandler]
